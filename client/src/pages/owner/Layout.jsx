7d834f18670dc8b3f8f2f333a3daea76b63bdc98
COSMICsk24 <shaswatkanungo1@gmail.com>
2025-07-28 12:38:11 +0530

    In progress

--- client/src/pages/owner/Layout.jsx
+++ client/src/pages/owner/Layout.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Sidebar from '../../components/owner/Sidebar';
+import NavbarOwner from '../../components/owner/NavbarOwner';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col min-h-screen'>
+      <NavbarOwner />
+
+      <div className='flex flex-1'>
+          <Sidebar />
+          <Outlet />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Layout
